669c8df7840b5a9c2b7aa0f5c9b9bace7b399ec6
christopher.hansen <christopher.hansen@porschedigitallab.com>
2018-04-24 16:58:11 +0200

    added main class

---
+++ src/hachristopher/ols/network/OLS.java
@@ -0,0 +1,30 @@
+package hachristopher.ols.network;
+
+public class OLS {
+
+	
+	double [] inData;
+	
+	
+	/**
+	 * Initalize all arrays and weights with random values and 
+	 */
+	private void initalize() {
+		
+	}
+	
+	
+	/**
+	 * 
+	 */
+	public void activateForeward() {
+		
+	}
+	
+	/**
+	 * Starts the training of the network
+	 */
+	public void start() {
+		initalize();
+	}
+}
