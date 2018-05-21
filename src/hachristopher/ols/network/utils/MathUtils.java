b3e9e308f675442e082e6d1943bc94d9bfd13b00
christopher.hansen <christopher.hansen@porschedigitallab.com>
2018-05-21 19:07:11 +0200

    added border function

--- src/hachristopher/ols/network/utils/MathUtils.java
+++ src/hachristopher/ols/network/utils/MathUtils.java
@@ -61,7 +61,7 @@ public class MathUtils {
 
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[0].length; j++) {
-				matrix[i][j] = a.invert(matrix[i][j]);
+				matrix[i][j] = a.invert(a.border(matrix[i][j]));
 			}
 		}
 
