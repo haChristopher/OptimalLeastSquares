fd29315d0b7a48e4140e6e55da838dcc08c2b82f
christopher.hansen <christopher.hansen@porschedigitallab.com>
2018-05-21 18:22:33 +0200

    improved dataset class and edited gitignore

--- src/hachristopher/ols/dataset/Datasets.java
+++ src/hachristopher/ols/dataset/Datasets.java
@@ -6,74 +6,86 @@ import java.io.IOException;
 
 public enum Datasets {
 	XOR, SMALLSPIRAL, BIGSPIRAL, MNIST;
-	
-	public double[][] x;
-	public double[][] y;
+
+	private double[][] x;
+	private double[][] y;
 
 	public void load() {
 		switch (this) {
-		case XOR : loadXor(); 
-				   break;
-		case SMALLSPIRAL: loadCSV("smallSpiralData.txt");
-						  break;
-		case BIGSPIRAL: loadCSV("bigSpiralData.txt");
-					    break;
-		case MNIST: loadXor();
-					break;
+		case XOR:
+			loadXor();
+			break;
+		case SMALLSPIRAL:
+			loadCSV("smallSpiralData.txt");
+			break;
+		case BIGSPIRAL:
+			loadCSV("bigSpiralData.txt");
+			break;
+		case MNIST:
+			loadXor();
+			break;
 		default:
 			throw new AssertionError("Unknown dataset " + this);
 		}
 	}
 
 	private void loadXor() {
-		double[][] data = {{0.01, 0.01}, {0.99, 0.01}, {0.01, 0.99}, {1.0, 1.0}};
-		double outData[] = {-0.5, 0.5, 0.5, -0.5};
+		double[][] data = { { 0.01, 0.01 }, { 0.99, 0.01 }, { 0.01, 0.99 }, { 1.0, 1.0 } };
+		double outData[] = { -0.5, 0.5, 0.5, -0.5 };
 		this.x = data;
 		this.y = transform(outData);
 	}
-	
-	public void loadCSV(String filename) {
 
-        String path = "src/" + filename  ;
-        String line = "";
-        String cvsSplitBy = ",";
-        	
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-        		int count = 0;
-        			int numData = Integer.parseInt(br.readLine());
-                double NewInData[][] = new double[numData][2];
-                double NewOutData[] = new double[numData];
-                
-            while ((line = br.readLine()) != null && count < numData) {
-                String[] data = line.split(cvsSplitBy);
-                NewInData[count][0] = Double.parseDouble(data[0]);
-                NewInData[count][1] = Double.parseDouble(data[1]);
-                NewOutData[count] = Double.parseDouble(data[2]);
-                count++;
-            }
-            
-        this.x = NewInData;
-        this.y = transform(NewOutData);
-            
+	private void loadCSV(String filename) {
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		String path = "src/" + filename;
+		String line = "";
+		String cvsSplitBy = ",";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			int count = 0;
+			int numData = Integer.parseInt(br.readLine());
+			double NewInData[][] = new double[numData][2];
+			double NewOutData[] = new double[numData];
+
+			while ((line = br.readLine()) != null && count < numData) {
+				String[] data = line.split(cvsSplitBy);
+				NewInData[count][0] = Double.parseDouble(data[0]);
+				NewInData[count][1] = Double.parseDouble(data[1]);
+				NewOutData[count] = Double.parseDouble(data[2]);
+				count++;
+			}
+
+			this.x = NewInData;
+			this.y = transform(NewOutData);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
-	
-	
+
 	/**
 	 * transforms one dimensional data into two dimensional data
-	 * @param one dimensional data
+	 * 
+	 * @param one
+	 *            dimensional data
 	 * @return two dimensional data
 	 */
-	private double[][] transform(double[] data){
+	private double[][] transform(double[] data) {
 		double[][] transformed = new double[data.length][1];
 		for (int i = 0; i < data.length; i++) {
-			transformed[i][0] =  data[i];
+			transformed[i][0] = data[i];
 		}
 		return transformed;
+	}
+
+	public double[][] getX() {
+		return x;
+	}
+
+	public double[][] getY() {
+		return y;
 	}
 
 }
